e8e7229d50410537f03de0f01b4f6994b32838dd
Winter <apkgamming@gmail.com>
2026-02-18 22:57:03 +0700

    check charr

---
+++ checkchar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char alphaUpper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    char alphaLower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int countUpper=0,countLower=0;
+    string user_input;
+    cin >> user_input;
+    
+    for(int x : user_input)
+
+}
