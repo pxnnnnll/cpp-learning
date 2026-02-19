f10fa3dfcb43358cbda325e24911f8f7c75dae33
Winter <apkgamming@gmail.com>
2026-02-19 15:21:29 +0700

    upper&lower string

--- checkchar.cpp
+++ checkchar.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
+#include<cctype>
 using namespace std;
 
 int main() {
-    char alphaUpper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    char alphaLower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     int countUpper=0,countLower=0;
     string user_input;
     cin >> user_input;
     
-    for(int x : user_input)
+    for(char x : user_input) {
+        if(isupper(x)){
+            countUpper+=1;
+        }
+        if(islower(x)){
+            countLower+=1;
+        }
+    }
+    
+    if(countUpper > 1 && countLower > 1) {
+        cout << "Mix";
+    }else if(countUpper > 1 && countLower == 0){
+        cout << "All capital Letter";
+    }else if(countUpper == 0 && countLower > 1){
+        cout << "All small Letter";
+    }
 
 }
